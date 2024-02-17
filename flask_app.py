b20da32283bd64ec117ff2dec30d05b31e50b58c
Mehtap Yercel <myercel315@gmail.com>
2024-02-17 12:03:16 -0500

    working on website

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+
 def index():
     return render_template('index.html')
+
+
+def read_csv():
+    return
 
 if __name__ == '__main__':
     app.run()
